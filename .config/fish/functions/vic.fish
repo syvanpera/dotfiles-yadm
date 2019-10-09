eb0d44c4a108132ab681c700fdfefa3b952243be
Tuomo Syvänperä <tuomo.syvanpera@gmail.com>
2019-10-09 08:32:21 +0300

    Update vic func

--- .config/fish/functions/vic.fish
+++ .config/fish/functions/vic.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
 function vic --description alias\ vic\ cat\ \~/scripts/config-files.csv\ \|\ sed\ \'s/,/\ /\'\ \|\ awk\ \'\{print\ \$2\}\'\ \|\ fzf\ \|\ sed\ \"s\|\\\~\|\$HOME\|\"\ \|\ xargs\ -I\{\}\ nvim\ \{\}
-	cat ~/scripts/config-files.csv | sed 's/,/ /' | awk '{print $2}' | fzf | sed "s|\~|$HOME|" | xargs -I{} nvim {} $argv;
+	cat ~/scripts/rofi/config-files.csv | sed 's/,/ /' | awk '{print $2}' | fzf | sed "s|\~|$HOME|" | xargs -I{} nvim {} $argv;
 end
